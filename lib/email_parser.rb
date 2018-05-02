849b2db8b68a276a5fdec4b2c7c58e6b925276f1
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-02 03:28:16 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ end
 
 def parse
   emails_without_duplicates =[]
-  emails_array = email.split(/\,\s|\"\s\s|\"\,|\"|\s/)
+  emails_array = emails.split(/\,\s|\"\s\s|\"\,|\"|\s/)
   emails_array.each do |email|
     if !emails_without_duplicates.include?(email)
       emails_without_duplicates.push(email)
